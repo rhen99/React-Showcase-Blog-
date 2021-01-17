84f0d3f02c6c599132d9ff0beb8de7f655ac39c4
Arhen Santiago <lincoldarhen9900@gmail.com>
2021-01-17 19:08:28 +0800

    blog page created

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,13 @@
+import BlogItem from "../BlogItem/BlogItem";
 import "./Blog.scss";
 function Blog() {
     return (
         <>
             <div className="container blog">
                 <h1 className="blog-title">Blog Posts</h1>
+                <BlogItem/>
+                <BlogItem/>
+                <BlogItem/>
             </div>   
         </>
     )
---
+++ src/components/BlogItem/BlogItem.js
@@ -0,0 +1,14 @@
+import "./BlogItem.scss";
+
+function BlogItem() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <a href="#" className="blog-post-title"><h3 className="card-title">Post Title</h3></a>
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum provident consequatur aperiam quo et eveniet neque quasi veritatis iure eligendi!</p>
+            </div>
+        </div>
+    )
+}
+
+export default BlogItem
